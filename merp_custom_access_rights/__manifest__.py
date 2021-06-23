5b5ef51f74621b45fa7833f4a03db3f247bf01e4
Yuri Morozov <yuri.m@ventor.tech>
2021-06-23 19:30:52 +0300

    [IMP] Added new version

--- merp_custom_access_rights/__manifest__.py
+++ merp_custom_access_rights/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Ventor Custom Access Rights',
-    "version": "12.0.1.0.1",
+    "version": "12.0.1.0.2",
     'author': 'VentorTech',
     'website': 'https://ventor.tech/',
     'license': 'LGPL-3',
