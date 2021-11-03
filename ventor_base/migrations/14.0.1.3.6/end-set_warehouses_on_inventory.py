eb1d13a29d333697f953d971ce87653a8b54eeba
Kirill <kirill.v@ventor.tech>
2021-11-03 16:49:45 +0300

    [REF] changed domain for search stock inventory

--- ventor_base/migrations/14.0.1.3.6/end-set_warehouses_on_inventory.py
+++ ventor_base/migrations/14.0.1.3.6/end-set_warehouses_on_inventory.py
@@ -6,10 +6,13 @@ def migrate(cr, version):
     """Update warehouse_id for all stock inventory"""
 
     env = api.Environment(cr, SUPERUSER_ID, {})
-    all_stock_inventory = env['stock.inventory'].search([
-            ('state', 'in', ('draft', 'confirm', 'cancel', 'done')),
-            ])
-    for stock_inv in all_stock_inventory:
+    all_stock_inventory_ids = env["stock.inventory"].search(
+        [
+            ("location_ids", "!=", False),
+        ]
+    )
+
+    for stock_inv in all_stock_inventory_ids:
         warehouse_ids = stock_inv.location_ids.mapped("warehouse_id")
         if len(warehouse_ids) == 1:
             stock_inv.warehouse_id = warehouse_ids
