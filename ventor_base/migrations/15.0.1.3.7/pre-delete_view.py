dfbd40ec2111c272823a18da5f6ca107d205efee
VentorTech <10652233+ventor-dev@users.noreply.github.com>
2021-12-22 11:29:11 +0100

    Fix migration

--- ventor_base/migrations/15.0.1.3.7/pre-delete_view.py
+++ ventor_base/migrations/15.0.1.3.7/pre-delete_view.py
@@ -3,5 +3,5 @@ def migrate(cr, version):
         return
 
     cr.execute("""
-        DELETE FROM ir_ui_view WHERE name='Users Preferences';
+        DELETE FROM ir_ui_view WHERE id in (select res_id from ir_model_data where name = 'res_user_form' and module='ventor_base');
     """)
