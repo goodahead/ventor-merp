898098714b56607ffc5124706a21a6682415147e
kirill <kirill.v@ventor.tech>
2022-02-21 07:56:29 +0000

    [REM] removed module ventor_picking_management

--- ventor_picking_management/README.rst
+++
@@ -1,2 +0,0 @@
-Picking management PRO
-=========================
--- ventor_picking_management/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright 2021 VentorTech OU
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0).
-
-from . import stock_picking_batch_rule
