77df025c301327ed1231205c739b5f6dfff2cf99
kirill <kirill.v@ventor.tech>
2022-03-03 11:37:18 +0000

    [REM] deleted batch picking rule for inventory user and add sudo to get wh

--- ventor_base/models/__init__.py
+++ ventor_base/models/__init__.py
@@ -4,7 +4,6 @@
 from . import stock_location
 from . import ventor_config
 from . import stock_picking
-from . import stock_picking_batch
 from . import res_company
 from . import res_config
 from . import res_users
