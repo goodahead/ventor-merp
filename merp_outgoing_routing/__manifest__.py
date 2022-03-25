c3f9c7724ee639535fff351ca58ca1024f83f839
kirill <kirill.v@ventor.tech>
2022-03-25 11:31:49 +0000

    [ADD] added license in manifest

--- merp_outgoing_routing/__manifest__.py
+++ merp_outgoing_routing/__manifest__.py
@@ -23,6 +23,7 @@
     "version": "10.0.1.0.1",
     'author': 'Ventor, Xpansa Group',
     'website': 'https://ventor.tech/',
+    'license': 'LGPL-3',
     'installable': True,
     'images': ['static/description/main_banner.png'],
     'description': """
