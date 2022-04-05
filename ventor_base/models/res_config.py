4f32fd03affd5920c8e0c554657e3cee44ec42c6
kirill <kirill.v@ventor.tech>
2022-04-05 18:23:55 +0000

    [REM] removed changes for logo loading

--- ventor_base/models/res_config.py
+++ ventor_base/models/res_config.py
@@ -1,10 +1,8 @@
 ﻿# Copyright 2020 VentorTech OU
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0).
 
-from odoo import models, fields, api, _, tools
+from odoo import models, fields, api, _
 from odoo.exceptions import UserError
-from PIL import Image
-import io
 import base64
 import struct
 import logging
@@ -117,25 +115,12 @@ class VentorConfigSettings(models.TransientModel):
             return False
 
         dat = base64.decodebytes(self.logotype_file)
-
-        image = Image.open(io.BytesIO(dat))
-        if image.format.lower() != 'png':
-            raise UserError(
-                _(
-                    "Apparently, the logotype is not a .png file"
-                    " or the file was incorrectly converted to .png format"
-                )
-            )
+        png = (dat[:8] == b'\211PNG\r\n\032\n' and (dat[12:16] == b'IHDR'))
+        if not png:
+            raise UserError(_('Apparently, the logotype is not a .png file.'))
 
         width, height = struct.unpack('>LL', dat[16:24])
         if int(width) < LOGOTYPE_W or int(height) < LOGOTYPE_H:
             raise UserError(_('The logotype can\'t be less than {}x{} px.'.format(LOGOTYPE_W, LOGOTYPE_H)))
 
         return True
-
-    @api.model
-    def create(self, values):
-        if values.get('logotype_file'):
-            tools.base64_to_image(values.get('logotype_file'))
-        res = super().create(values)
-        return res
