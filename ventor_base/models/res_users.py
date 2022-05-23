84245337bd778bb928f2046c8b06e4fac2f51106
kirill <kirill.v@ventor.tech>
2022-05-23 12:14:13 +0000

    [ADD] added field for user ventor_base_version

--- ventor_base/models/res_users.py
+++ ventor_base/models/res_users.py
@@ -15,6 +15,12 @@ class ResUsers(models.Model):
         help='List of all warehouses user has access to',
     )
 
+    ventor_base_version = fields.Char(
+        compute="_compute_ventor_base_version",
+        compute_sudo=True,
+        readonly=True,
+    )
+
     ventor_global_settings = fields.Text(
         string='Global Settings',
         readonly=True,
@@ -31,6 +37,7 @@ class ResUsers(models.Model):
             'ventor_global_settings',
             'ventor_user_settings',
             'custom_package_name',
+            'ventor_base_version',
         ]
         return super().SELF_READABLE_FIELDS + readable_fields
 
@@ -38,6 +45,18 @@ class ResUsers(models.Model):
     def SELF_WRITEABLE_FIELDS(self):
         writable_fields = ['ventor_user_settings']
         return super().SELF_WRITEABLE_FIELDS + writable_fields
+
+    def _compute_ventor_base_version(self):
+        ventor_base_version = (
+            self.env["ir.module.module"]
+            .search([("name", "=", "ventor_base"), ("state", "=", "installed")])
+            .latest_version
+        )
+        for user in self:
+            if ventor_base_version:
+                user.ventor_base_version = ventor_base_version
+            else:
+                user.ventor_base_version = ""
 
     def _compute_global_settings(self):
         settings = []
