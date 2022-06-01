f5bcac65483fe8282952e711c0d1bc91bb27681d
kirill <kirill.v@ventor.tech>
2022-06-01 17:53:47 +0000

    [REM] removed checking group from action_validate

--- ventor_base/models/stock_inventory.py
+++ ventor_base/models/stock_inventory.py
@@ -33,8 +33,8 @@ class StockInventory(models.Model):
         # we need to override method as we need different access group
         # to be allowed to validate inventory
         if (
-            groups == "stock.group_stock_manager"
-            and self.user_has_groups("ventor_base.merp_user_validate_inventory_adjustment")
+            self.env.context.get("validate_inventory")
+            and groups == "stock.group_stock_manager"
         ):
             groups = "ventor_base.merp_user_validate_inventory_adjustment"
         res = super(StockInventory, self).user_has_groups(groups)
@@ -49,6 +49,10 @@ class StockInventory(models.Model):
     def write(self, vals):
         res = super(StockInventory, self).write(vals)
         self._update_location()
+        return res
+
+    def action_validate(self):
+        res = super(StockInventory, self.with_context(validate_inventory=True)).action_validate()
         return res
 
 
