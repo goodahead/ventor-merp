446d51f8e099a0bc46cff2a40f74b4997664af59
kirill <kirill.v@ventor.tech>
2022-09-28 19:58:01 +0000

    [MERPWH-1093] udpated migrations

--- ventor_base/migrations/15.0.1.5.0/post-update_ventor_settings.py
+++ ventor_base/migrations/15.0.1.5.0/post-update_ventor_settings.py
@@ -23,8 +23,28 @@ def migrate(cr, version):
         ).set_related_package_fields(group_settings.get('group_stock_tracking_lot'))
 
     ventor_roles_administrator = env.ref('ventor_base.ventor_role_admin')
+    ventor_roles_manager = env.ref('ventor_base.ventor_role_wh_manager')
+    ventor_roles_worker = env.ref('ventor_base.ventor_role_wh_worker')
     ventor_roles_administrator.write(
         {
             'implied_ids': [(4, env.ref('ventor_base.merp_manage_ventor_configuration_app').id)],
         }
     )
+
+    users = env['res.users'].with_context(active_test=False).search([
+        ('share', '=', False)
+        ])
+
+    for user in users:
+        if ventor_roles_manager not in user.groups_id and ventor_roles_worker in user.groups_id:
+            user.write(
+                {
+                    'groups_id': [(3, env.ref("ventor_base.merp_menu_allow_changing_settings").id)]
+                }
+            )
+        if user.ventor_user_settings:
+            user.write(
+                {
+                    'groups_id': [(4, env.ref("ventor_base.merp_menu_use_local_user_device_settings").id)]
+                }
+            )
