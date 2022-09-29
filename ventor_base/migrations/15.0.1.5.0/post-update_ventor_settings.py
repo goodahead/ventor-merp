a35fdc3251bb91e9c69e01a05b75eecba2fc1be7
kirill <kirill.v@ventor.tech>
2022-09-29 10:01:31 +0000

    [MERPWH-1093][IMP] improved condition for determining group

--- ventor_base/migrations/15.0.1.5.0/post-update_ventor_settings.py
+++ ventor_base/migrations/15.0.1.5.0/post-update_ventor_settings.py
@@ -23,8 +23,6 @@ def migrate(cr, version):
         ).set_related_package_fields(group_settings.get('group_stock_tracking_lot'))
 
     ventor_roles_administrator = env.ref('ventor_base.ventor_role_admin')
-    ventor_roles_manager = env.ref('ventor_base.ventor_role_wh_manager')
-    ventor_roles_worker = env.ref('ventor_base.ventor_role_wh_worker')
     ventor_roles_administrator.write(
         {
             'implied_ids': [(4, env.ref('ventor_base.merp_manage_ventor_configuration_app').id)],
@@ -36,7 +34,9 @@ def migrate(cr, version):
         ])
 
     for user in users:
-        if ventor_roles_manager not in user.groups_id and ventor_roles_worker in user.groups_id:
+        if not user.has_group("ventor_base.ventor_role_wh_manager") and user.has_group(
+                "ventor_base.ventor_role_wh_worker"
+        ):
             user.write(
                 {
                     'groups_id': [(3, env.ref("ventor_base.merp_menu_allow_changing_settings").id)]
