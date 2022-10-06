85fd3833f56daccf708670c8dc8e1ea1033b813c
kirill <kirill.v@ventor.tech>
2022-10-06 12:53:28 +0000

    migrationmail_reply_to_sender_globald from version 15 to version 16

--- mail_reply_to_sender_global/__manifest__.py
+++ mail_reply_to_sender_global/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Mail Reply to Sender Global",
     "summary": "Emails in Odoo are sent to generic mail address",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Tools",
     "website": "https://ventor.tech/",
     "author": "VentorTech OU",
