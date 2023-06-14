25f2cfbe15962a6165b1fbf8b904ebeca1991992
kirill <kirill.v@ventor.tech>
2023-06-14 16:24:12 +0000

    [MERPWH-1276] performance improvement serialize_record_ventor

--- outgoing_routing/models/stock_picking_mixin.py
+++ outgoing_routing/models/stock_picking_mixin.py
@@ -19,55 +19,57 @@ class StockPickingMixin(models.AbstractModel):
         related='company_id.routing_module_version',
     )
 
-    @staticmethod
-    def _recheck_record_list(record_list):
-        rechecked_list = []
-        for rec in record_list:
-            if rec.get('_type') == 'stock.package_level' and rec.get('is_done'):
-                continue
-            rechecked_list.append(rec)
-        return rechecked_list
+    def _get_stock_object(self, rec_id):
+        try:
+            return self.search([('id', '=', int(rec_id))])
+        except Exception as ex:
+            _logger.error(ex)
+            return False
 
-    @staticmethod
-    def _get_field(record, package_fields, operation_fields):
-        if record._name == 'stock.package_level':
-            return package_fields
-        return operation_fields
+    def _get_sml_with_entire_pack(self, operations_to_pick):
+        return operations_to_pick.filtered(lambda x: x.package_level_id and not x.package_level_id.is_done)
 
-    @staticmethod
-    def _get_full_list(stock_object, limit):
+    def _get_sml_data(self, sml_ids, operation_fields):
+        sml_data = sml_ids.read(fields=operation_fields)
+        [rec.update({'_type': 'stock.move.line'}) for rec in sml_data]
+        return sml_data
+
+    def _get_package_level_data(self, sml_ids, package_fields):
+        package_level_data = sml_ids.mapped('package_level_id').read(fields=package_fields)
+        [rec.update({'_type': 'stock.package_level'}) for rec in package_level_data]
+        return package_level_data
+
+    def _serialize_picking_data(self, stock_object, package_fields, operation_fields, limit=None):
+        picking_data = []
         operations_to_pick = stock_object.operations_to_pick
+
         if limit:
             operations_to_pick = stock_object.operations_to_pick[:limit]
-        return [rec._get_operation_tuple() for rec in operations_to_pick]
 
-    def _read_record(self, record_tuple, package_fields, operation_fields):
-        """
-        record_tuple = (
-            ('id', 100),
-            ('_type', 'stock.move.line'),
-        )
+        # If entire pack is not used, return only stock move lines without packages
+        is_using_entire_pack = stock_object.picking_type_id.show_entire_packs
+        if not is_using_entire_pack:
+            sml_data = self._get_sml_data(operations_to_pick, operation_fields)
+            picking_data += sml_data
+            return picking_data
 
-        id:: number (int)
-        _type:: 'stock.move.line' or 'stock.package_level' (str)
-        """
-        record_dict = dict(record_tuple)
-        record = self.env[record_dict['_type']].browse(record_dict['id'])
-        record_dict.update(record.read(self._get_field(record, package_fields, operation_fields))[0])
-        return record_dict
+        # Separation of stock move lines with entire pack and without
+        sml_with_entire_pack_ids = self._get_sml_with_entire_pack(operations_to_pick)
+        sml_without_entire_pack_ids = operations_to_pick - sml_with_entire_pack_ids
+
+        sml_data = self._get_sml_data(sml_without_entire_pack_ids, operation_fields)
+        package_level_data = self._get_package_level_data(sml_with_entire_pack_ids, package_fields)
+
+        picking_data += sml_data
+        picking_data += package_level_data
+
+        return picking_data
 
     def serialize_record_ventor(self, rec_id, package_fields=[], operation_fields=[], limit=None):
         """Record serialization for the Ventor app."""
-        filtered_list = []
-        try:
-            stock_object = self.search([
-                ('id', '=', int(rec_id)),
-            ])
-        except Exception as ex:
-            _logger.error(ex)
-            return filtered_list
+        stock_object = self._get_stock_object(rec_id)
+        if not stock_object:
+            return []
 
-        full_list = self._get_full_list(stock_object, limit)
-        [filtered_list.append(rec) for rec in full_list if rec not in filtered_list]
-        record_list = [self._read_record(rec, package_fields, operation_fields) for rec in filtered_list]
-        return self._recheck_record_list(record_list)
+        picking_data = self._serialize_picking_data(stock_object, package_fields, operation_fields, limit)
+        return picking_data
