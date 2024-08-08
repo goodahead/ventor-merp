b5e9eaefdc1aca0cbb598966ef9683a8b3b61b0e
kirill <kirill.v@ventor.tech>
2024-08-08 13:18:08 +0000

    [MERPWH-1425] Updated the description for setting 'Show packages field' in the Instant Inventory menu

--- ventor_base/__manifest__.py
+++ ventor_base/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Ventor Base',
-    'version': '17.0.1.6.0',
+    'version': '17.0.1.7.0',
     'author': 'VentorTech',
     'website': 'https://ventor.tech/',
     'license': 'LGPL-3',
